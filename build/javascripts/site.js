221128012453c70373d933797d5a024267cbd5c0
alexandre cousin <cousin.a@laposte.net>
2019-07-25 21:27:12 +0200

    full responsive mobile, correction du contact form et de la page agence

--- build/javascripts/site.js
+++ build/javascripts/site.js
@@ -1,3 +1,10 @@
 // This is where it all goes :)
 
-
+function myFunction() {
+ const menu = document.querySelector(".menu-mobile-bot");
+  if (menu.id === "hidden") {
+    menu.id -= "hidden";
+  } else {
+    menu.id = "hidden";
+  }
+}
